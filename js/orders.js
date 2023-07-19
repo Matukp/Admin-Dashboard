bf2ccd66a982cc1927526d3fb50a5ede555a7aa4
Matias Marchsese <102271123+Matukp@users.noreply.github.com>
2023-07-18 23:06:10 -0300

    Admin Dashboard 1.0.0

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'sneakers',
+        productNumber: '123',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'socks',
+        productNumber: '456',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'hose',
+        productNumber: '789',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    }
+]
